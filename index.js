d04cecd533be397874f5b3fa616f9b95d98a3f5d
k-dot-11 <karnatheveer@gmail.com>
2021-04-24 12:09:31 +0530

    new

--- index.js
+++ index.js
@@ -1,55 +1,97 @@
 const fs = require('fs');
-const readline = require('readline');
 const { google } = require('googleapis');
 const open = require('open');
-const fetch = require('node-fetch');
+const express = require('express');
+
+var cors = require('cors')
+const app = express()
+ 
+app.use(cors())
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded());
 
 const SCOPES = [ 'https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events' ];
 
-const TOKEN_PATH = 'token.json';
+var admin = require('firebase-admin');
 
-fs.readFile('credentials.json', (err, content) => {
-	if (err) return console.log('Error loading client secret file:', err);
-	authorize(JSON.parse(content), createEvent);
+var serviceAccount = require('./firebasecreds.json');
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount)
+});
+const db = admin.firestore();
+
+app.post('/create-event', (req, res) => {
+	fs.readFile('credentials.json', (err, content) => {
+		if (err) return console.log('Error loading client secret file:', err);
+		authorize(JSON.parse(content), createEvent, req.body.email);
+		res.end('hmmmm');
+	});
 });
 
-function authorize(credentials, callback) {
+app.post('/create-user', (req, res) => {
+	fs.readFile('credentials.json', (err, content) => {
+		if (err) return console.log('Error loading client secret file:', err);
+		createUser(JSON.parse(content));
+		res.end('hmmmm');
+	});
+});
+
+app.post('/set-token', (req, res) => {
+	fs.readFile('credentials.json', (err, content) => {
+		if (err) return console.log('Error loading client secret file:', err);
+		setToken(JSON.parse(content), req.body.authcode, req.body.email);
+		res.end('hmmmm');
+	});
+});
+
+function authorize(credentials, call, email) {
 	const { client_secret, client_id, redirect_uris, response_type } = credentials.installed;
 	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0], response_type);
-	//look for email in firestore tokens collection
-	//if email is there get the token, set the token, callback
-	
-	fs.readFile(TOKEN_PATH, (err, token) => {
-		if (err) return getAccessToken(oAuth2Client, callback);
-		oAuth2Client.setCredentials(JSON.parse(token));
-		callback(oAuth2Client);
+
+	const usersRef = db.collection('tokens').doc(email);
+
+	usersRef.get().then((docSnapshot) => {
+		if (docSnapshot.exists) {
+			usersRef.onSnapshot((doc) => {
+				oAuth2Client.setCredentials(doc.data());
+				call(oAuth2Client);
+			});
+		} else {
+			res.send('doesnt exist');
+		}
 	});
 }
 
-function getAccessToken(oAuth2Client, callback) {
+function createUser(credentials, call, email) {
+	const { client_secret, client_id, redirect_uris, response_type } = credentials.installed;
+	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0], response_type);
+	getAccessToken(oAuth2Client);
+}
+
+function getAccessToken(oAuth2Client) {
 	const authUrl = oAuth2Client.generateAuthUrl({
 		access_type: 'online',
 		scope: SCOPES,
 		response_type: 'code'
 	});
-	open(authUrl)
-	const rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
+	open(authUrl);
+	
+}
+
+function setToken( credentials,code,email) {
+	const { client_secret, client_id, redirect_uris, response_type } = credentials.installed;
+	const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0], response_type);
+	oAuth2Client.getToken(code, (err, token) => {
+		if (err) return console.error('Error retrieving access token', err);
+		oAuth2Client.setCredentials(token);
+		docRef = db.collection('tokens').doc(email);
+
+		docRef.set(token);
 	});
-	rl.question('Enter the code from that page here: ', (code) => {
-		rl.close();
-		oAuth2Client.getToken(code, (err, token) => {
-			if (err) return console.error('Error retrieving access token', err);
-			oAuth2Client.setCredentials(token);
-			// Store the token to disk for later program executions
-			fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-				if (err) return console.error(err);
-				console.log('Token stored to', TOKEN_PATH);
-			});
-			callback(oAuth2Client);
-		});
-	});
+	
 }
 
 async function createEvent(auth) {
@@ -89,33 +131,64 @@ async function createEvent(auth) {
 	);
 }
 
-function listEvents(auth) {
-	const calendar = google.calendar({ version: 'v3', auth });
-	calendar.events.list(
-		{
-			calendarId: 'primary',
-			timeMin: new Date().toISOString(),
-			maxResults: 16,
-			singleEvents: true,
-			orderBy: 'startTime'
-		},
-		(err, res) => {
-			if (err) return console.log('The API returned an error: ' + err);
-			const events = res.data.items;
-			if (events.length) {
-				console.log('Upcoming 10 events:');
-				events.map((event, i) => {
-					const start = event.start.dateTime || event.start.date;
-					console.log(`${start} - ${event.summary}`);
-				});
-			} else {
-				console.log('No upcoming events found.');
-			}
-		}
-	);
-}
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
+module.exports = app;
 
-module.exports = {
-	SCOPES,
-	listEvents
-};
+
+
+
+
+// function listEvents(auth) {
+// 	const calendar = google.calendar({ version: 'v3', auth });
+// 	calendar.events.list(
+// 		{
+// 			calendarId: 'primary',
+// 			timeMin: new Date().toISOString(),
+// 			maxResults: 16,
+// 			singleEvents: true,
+// 			orderBy: 'startTime'
+// 		},
+// 		(err, res) => {
+// 			if (err) return console.log('The API returned an error: ' + err);
+// 			const events = res.data.items;
+// 			if (events.length) {
+// 				console.log('Upcoming 10 events:');
+// 				events.map((event, i) => {
+// 					const start = event.start.dateTime || event.start.date;
+// 					console.log(`${start} - ${event.summary}`);
+// 				});
+// 			} else {
+// 				console.log('No upcoming events found.');
+// 			}
+// 		}
+// 	);
+// }
+
+// app.post('/put', (req, res) => {
+// docRef = db.collection('tokens').doc('alovelace');
+
+// docRef.set({
+// 	first: 'Ada',
+// 	last: 'Lovelace',
+// 	born: 1815
+// });
+
+// 	res.end('bic')
+// });
+
+// app.get('/snap', (req, res) => {
+// 	const usersRef = db.collection('tokens').doc('alovelace');
+
+// 	usersRef.get().then((docSnapshot) => {
+// 		if (docSnapshot.exists) {
+// 			usersRef.onSnapshot((doc) => {
+// 				res.send(doc.data());
+// 			});
+// 		} else {
+// 			res.send('doesnt exist');
+// 		}
+// 	});
+// });
